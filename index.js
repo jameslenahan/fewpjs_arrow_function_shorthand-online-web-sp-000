47ac47fba38ceea60b6ebfd16c8c7c3b91efb126
James Lenahan <james.k.lenahan@gmail.com>
2020-06-30 03:33:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,43 +1,5 @@
-// Your code here
+let divide = x => 2000 / 100;
 
-function saturdayFun(activity = 'roller-skate'){
-    return `This Saturday, I want to ${activity}!`
-}
+let square = x => x*x;
 
-function mondayWork(activity = 'go to the office'){
-    return `This Monday, I will ${activity}.`
-}
-
-function wrapAdjective(flair = '*'){
-    let char = flair
-    return function (noun = 'special'){
-        return `You are ${char}${noun}${char}!`
-    }
-}
-
-function actionApplyer(starting_int, function_array){
-    let int = starting_int;
-    function_array.forEach(func => {
-        int = func(int)
-    });
-    return int
-}
-
-const Calculator = {
-
-    add: function(a, b){
-        return a + b;
-    },
-
-    subtract: function(a, b){
-        return a - b;
-    },
-
-    multiply: function(a, b){
-        return a * b;
-    },
-
-    divide: function(a, b){
-        return a / b;
-    }
-};
+let add = (x, y) => x + y;
